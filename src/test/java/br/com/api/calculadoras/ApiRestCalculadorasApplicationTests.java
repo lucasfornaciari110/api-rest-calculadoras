870b2324e80ffd9e7d493c4509db4e39b0e011cd
Lucas Fornaciari <lucascost910@hotmail.com>
2024-11-07 16:31:40 -0300

    Criação do Projeto

---
+++ src/test/java/br/com/api/calculadoras/ApiRestCalculadorasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.calculadoras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestCalculadorasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
